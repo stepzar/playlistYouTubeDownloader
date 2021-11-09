bdd05c6052b01541e4e399219f6573aee02c80d3
stepzar <stefanozarro@gmail.com>
2021-11-09 18:18:28 +0100

    license activation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import shutil
 import time
+import requests
+import json
 
 from pytube import YouTube, Playlist
 import subprocess
@@ -8,9 +10,6 @@ import threading
 
 def download_playlist(url):
     playlist = Playlist(url)
-
-    if not os.path.isdir("temp"):
-        os.mkdir("temp")
 
     cont = 1
     if os.path.isdir(playlist.title):
@@ -32,29 +31,63 @@ def download_playlist(url):
 
 
 def download_song_tomp3(song, playlist_title):
+    print(song.title)
+
     # download
     try:
-        song.streams.filter(only_audio=True)[4].download(output_path="temp", filename=song.title)
+        song.streams.filter(only_audio=True)[4].download(output_path=playlist_title, filename=song.title + ".mp3")
     except:
-        song.streams.filter(only_audio=True)[2].download(output_path="temp", filename=song.title)
+        song.streams.filter(only_audio=True)[2].download(output_path=playlist_title, filename=song.title + ".mp3")
 
     # convert
-    subprocess.run(["ffmpeg", "-i", "temp/" + song.title, playlist_title + "/" + song.title + ".mp3"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    #subprocess.run(["ffmpeg", "-i", "temp/" + song.title, playlist_title + "/" + song.title + ".mp3"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
     print("Downloaded: " + song.title)
 
+def activate(license):
+    url = "https://api.keygen.sh/v1/accounts/kitemmuort01/licenses/actions/validate-key"
+
+    payload = json.dumps({
+        "meta": {
+            "key": license,
+            "scope": {
+                "product": "cb718770-4c17-4ccc-b1ef-35ed772b866c",
+                "policy": "79c0f3cd-033e-4aaa-bfef-30f0de5d95cb",
+                "fingerprint": "39:57:94:94:48:98:01:83"
+            }
+        }
+    })
+
+    headers = {
+        'Content-Type': 'application/vnd.api+json',
+        'Accept': 'application/vnd.api+json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    if response.json()["data"] == None:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     print("Software created by stepzar, check my github: github.com/stepzar")
     print("\n\n")
-    link = input("Enter your video/playlist URL: ")
 
-    while not(link.startswith("https://www.youtube.com") or link.startswith("https://youtube.com")):
-        link = input("Enter a correct youtube URL: ")
+    license = input("Inserisci la chiave di licenza: ")
 
-    print("Downloading...\n\n")
-    if "playlist" in link:
-        download_playlist(link)
+    if activate(license):
+        link = input("Inserisci il link della playlist youtube: ")
 
-    print("\n\nDownload Complete! Thansk for using this product, by stepzar <3")
+        while not(link.startswith("https://www.youtube.com") or link.startswith("https://youtube.com")):
+            link = input("Inserisci un link corretto: ")
 
-    shutil.rmtree("temp")
+        print("Downloading...\n\n")
+        if "playlist" in link:
+            download_playlist(link)
+
+        print("\n\nDownload Completato! Grazie per avermi scelto, stepzar <3")
+
+        #shutil.rmtree("temp")
+    else:
+        print("Devi attivare la tua copia di Playlist Downloader")
